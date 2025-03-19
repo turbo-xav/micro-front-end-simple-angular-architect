bf5a2b1202dee79a5d13766dc042bcce4053a263
Turbo Xav <xavier.tagliarino@gmail.com>
2025-03-19 11:21:07 +0100

    refactor(mf): Consume react component with "@angular-architects/module-federation-tools"

--- projects/host-app/src/app/app.routes.ts
+++ projects/host-app/src/app/app.routes.ts
@@ -1,7 +1,19 @@
 import { Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/module-federation';
+import {WebComponentWrapper, WebComponentWrapperOptions} from '@angular-architects/module-federation-tools';
 
 export const routes: Routes = [
+  {
+    path: 'react',
+    component: WebComponentWrapper,
+    data: {
+      type: 'script',
+      remoteEntry: 'https://witty-wave-0a695f710.azurestaticapps.net/remoteEntry.js',
+      remoteName: 'react',
+      exposedModule: './web-components',
+      elementName: 'react-element'
+    } as WebComponentWrapperOptions
+  },
   {
     path: 'remote-app',
     loadComponent: () =>
